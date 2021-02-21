cc2db2eb26e03577639c5b3bba8fd14550d0314b
grace0950 <pinyan0950@gmail.com>
2021-02-21 20:34:43 +0800

    Express 基礎伺服器架構

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var app = express(); //產生 Express Application 物件
 
+/*
+  req: 獲取url訊息 request
+  res: 瀏覽器返回響應訊息 response
+*/
+
 app.get("/", function (req, res) { //當使用者連線到伺服器根目錄("/")時，做出回應
   res.send("<b>Welcome to Express!</b>");
 });
